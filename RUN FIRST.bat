00f82c9f7f13c9949eb5051b068f1ea15135420f
arffsaad <80538339+arffsaad@users.noreply.github.com>
2021-07-16 01:14:45 +0800

    copy usrdir to cwd
    allow driver to run independently without being disrupted by active chrome window

--- RUN FIRST.bat
+++ RUN FIRST.bat
@@ -4,5 +4,9 @@ IF NOT EXIST "%userprofile%\Downloads\chromedriver_win32.zip" bitsadmin /transfe
 set /p tme="Enter the time you wish to collect coins(in HH:MM, 24HR format): "
 SCHTASKS /CREATE /SC DAILY /TN "Autocoin\shopeecollect" /TR "%cd%\autocoin.py" /ST %tme%
 CLS
+ECHO Creating usrdata...
+MKDIR usrdata
+xcopy /s "%localappdata%\Google\Chrome\User Data" "%cd%\usrdata\"
+CLS
 ECHO First time setup done! Press enter to quit...
 PAUSE
