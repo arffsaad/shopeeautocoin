9c7c204a18cc1491063237408bcdf2b789511577
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2022-09-19 14:33:37 +0800

    Revamped autocoin
    replaced selenium with playwright, cleaner API and chromedriver is managed properly.
    
    Should be able to run crossplatform through terminal.

--- autocoin.py
+++ autocoin.py
@@ -1,72 +1,104 @@
-from time import sleep
-from selenium import webdriver
-from os import system
+#!/bin/bash python3
+
+from playwright.sync_api import sync_playwright
+from time import sleep as sl
 import os
 
-# reading params
-f = open('params.txt','r')
-pars = f.readlines()
+# persistence, load an existing user data dir.
+#user_data_dir = 'udd' #testing
+user_data_dir = 'persistence' #production
 
-# vars
+print("""
+    _   _   _ _____ ___   ____ ___ ___ _   _ 
+   / \ | | | |_   _/ _ \ / ___/ _ \_ _| \ | |
+  / _ \| | | | | || | | | |  | | | | ||  \| |
+ / ___ \ |_| | | || |_| | |__| |_| | || |\  |
+/_/   \_\___/  |_| \___/ \____\___/___|_| \_|
+""")
+print("========== Shopee Coin AutoCollector v2.0 ==========\n\n")
+sl(1)
+print("Checking credentials.txt...")
+sl(1)
+# check if file exists
+if not(os.path.isfile('credentials.txt')):
+    print("Credentials.txt does not exist. First login detected. Please follow prompts to login and authorize 2FA")
+    sl(1)
+    with sync_playwright() as p:
+        browser = p.chromium.launch_persistent_context(user_data_dir, headless=True)
+        # get current page
+        page = browser.pages[0]
+        page.goto('https://shopee.com.my/buyer/login?next=https%3A%2F%2Fshopee.com.my%2Fshopee-coins', wait_until = 'domcontentloaded')
+        page.reload()
+        # do login
+        try:
+            page.locator('xpath=//button[contains(text(), "English")]').click()
+        except:
+            nothing = 0
+        username = input("Enter Username(Or phone number): ")
+        password = input("Enter Password: ")
+        print("Logging in...")
+        sl(1)
+        page.locator('input[name="loginKey"]').fill(username)
+        page.locator('input[name="password"]').fill(password)
+        # press enter
+        page.keyboard.press('Enter')
+        print("Performing 2FA Authorization. Please check your Whatsapp, and click the link to authorize.\n")
+        sl(1)
+        page.locator('xpath=//div[contains(text(), "Authentication Link")]').click()
+        page.locator('xpath=//button[contains(text(), "OK")]').click()
+        input("Press Enter once you have authorized the login.")
+        page.goto('https://shopee.com.my')
+        # get text from navbar__username
+        print('Logged in as:', page.locator("#stardust-popover1 > div > div > div.navbar__username").inner_text())
+        print("Saving Credentials...")
+        with open('credentials.txt', 'w') as f:
+            f.write(username + '\n')
+            f.write(password)
+        sl(3)
+        input("First login succesful!\nCredentials saved in credentials.txt.\nPlease create a Scheduled Task (Windows) or Cron Job (Linux) to run this script every 24 hours.\nScript has finished.\n\nPress Enter to continue...")
+        browser.close()
 
-coin = pars[0]
-usr = pars[1]
-psw = pars[2]
-login = pars[3]
-amount = pars[4]
-total = pars[5]
-usrn = pars[6]
-pswd = pars[7]
-loginURL = pars[8][0:-1]
-coinURL = pars[9]
-driverpath = pars[10][0:-1]
-usrdir = ('--user-data-dir=' + os.getcwd() + r"\usrdata")
 
-f.close()
-# end var
-
-option = webdriver.ChromeOptions()
-option.add_argument(usrdir)
-option.add_argument(r"--profile-directory=Default")
-option.add_argument("--log-level=3")
-option.add_argument("--disable-extensions")
-option.add_argument("--disable-gpu")
-option.add_argument("--headless")
-option.add_argument('--window-size=1920,1080')
-option.add_argument('--remote-debugging-port=54332')
-
-driver = webdriver.Chrome(executable_path=driverpath, options=option)
-
-system('cls')
-print("Shopee Daily Autocoin Collector v1.0")
-sleep(0.5)
-print("by ssl2k")
-sleep(0.5)
-print("\nCollecting coins",end="")
-driver.get(coinURL)
-sleep(3)
-
-driver.find_element_by_xpath(coin).click()
-sleep(3)
-if (driver.current_url == loginURL):
-    driver.find_element_by_xpath(usr).send_keys(usrn)
-    driver.find_element_by_xpath(psw).send_keys(pswd)
-    driver.find_element_by_xpath(login).click()
-    sleep(3)
-    driver.find_element_by_xpath(coin).click()
-
-for x in range(3):
-    print(".", end="")
-    sleep(0.5)
-print("\nTotal coins as of now:", driver.find_element_by_xpath(total).text)
-amt = driver.find_element_by_xpath(amount)
-amt = int(amt.text[26])
-if (amt == 1):
-    amt = 7
 else:
-    amt = amt - 1
-
-
-print(amt, "Coins collected. Exiting...")
-sleep(3)
-driver.quit()
+    print("Credentials.txt found. Collecting coins...")
+    # get credentials
+    with open('credentials.txt', 'r') as f:
+        credentials = f.read().splitlines()
+        username = credentials[0]
+        password = credentials[1]
+        print("Credentials loaded.")
+    # do normal routine
+        with sync_playwright() as p:
+            browser = p.chromium.launch_persistent_context(user_data_dir, headless=True)
+            # get current page
+            page = browser.pages[0]
+            page.goto('https://shopee.com.my/buyer/login?next=https%3A%2F%2Fshopee.com.my%2Fshopee-coins', wait_until = 'domcontentloaded')
+            sl(2)
+            page.reload()
+            print("Checking if logged in...")
+            if (page.url == 'https://shopee.com.my/buyer/login?next=https%3A%2F%2Fshopee.com.my%2Fshopee-coins'):
+                # do login
+                print("Not logged in. Logging in...")
+                page.locator('input[name="loginKey"]').fill(username)
+                page.locator('input[name="password"]').fill(password)
+                # press enter
+                page.keyboard.press('Enter')
+            else:
+                # wait for dom, click get coin.
+                print("Logged in. Collecting...")
+            # click on xpath
+            try:
+                page.locator('xpath=//button[contains(text(), "Check in today")]').click()
+            except:
+                try:
+                    page.locator('xpath=//button[contains(text(), "Come back tomorrow")]').click()
+                    print("Coins already collected today.")
+                except:
+                    print("Unknown Exception.")
+            sl(2)
+            page.goto("https://shopee.com.my/user/coin/list")
+            coinbal = page.locator("#main > div > div.dYFPlI > div > div.xMDeox > div > div > div.NpdN3L > div.ZBdeXm").inner_text()
+            cointoday = page.locator("#main > div > div.dYFPlI > div > div.xMDeox > div > div > div:nth-child(2) > div.rXcU7s > div:nth-child(1) > div > div.R519Sm._5Q-g4s").inner_text()
+            print("\nCoins Collected!\nCoins balance: ", coinbal, "\nCoins Collected Today: ", cointoday, "\n\n")
+            input("Press Enter to exit...")
+            browser.close()
